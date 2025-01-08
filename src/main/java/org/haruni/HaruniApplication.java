bc5ef21d1e617655749db99e63c9eaf7ee2d4396
xunxxoie <ahh0520@naver.com>
2025-01-08 14:22:55 +0900

    [Feat] Initial Commit

---
+++ src/main/java/org/haruni/HaruniApplication.java
@@ -0,0 +1,13 @@
+package org.haruni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaruniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaruniApplication.class, args);
+    }
+
+}
