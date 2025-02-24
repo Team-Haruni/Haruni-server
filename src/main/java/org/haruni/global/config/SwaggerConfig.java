55a4656e34f9d4a8d5d08cf2bb0251358ec3cfef
xunxxoie <ahh0520@naver.com>
2025-02-24 22:46:29 +0900

    [Fix] SwaggerConfig serverUrl Update

--- src/main/java/org/haruni/global/config/SwaggerConfig.java
+++ src/main/java/org/haruni/global/config/SwaggerConfig.java
@@ -4,8 +4,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -21,12 +24,16 @@ public class SwaggerConfig {
 
         SecurityRequirement securityRequirement = new SecurityRequirement().addList("BearerAuth");
 
+        Server server = new Server();
+        server.setUrl("https://actserver.site");
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Haruni API Docs")
                         .description("Capstone Design Team Haruni API Docs")
                         .version("v1")
                 )
+                .servers(List.of(server))
                 .addSecurityItem(securityRequirement)
                 .schemaRequirement("BearerAuth", securityScheme);
     }
