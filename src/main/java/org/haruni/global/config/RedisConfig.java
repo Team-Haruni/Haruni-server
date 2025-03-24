b383b13bdc8ca09a80bdec08c857ad461e77cfed
xunxxoie <ahh0520@naver.com>
2025-03-24 14:13:40 +0900

    [Fix] HashOperations 빈 등록

--- src/main/java/org/haruni/global/config/RedisConfig.java
+++ src/main/java/org/haruni/global/config/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -45,5 +46,10 @@ public class RedisConfig {
         redisTemplate.setConnectionFactory(redisConnectionFactory());
 
         return redisTemplate;
+    }
+
+    @Bean
+    public HashOperations<String, String, String> hashOperations(RedisTemplate<String, String> redisTemplate) {
+        return redisTemplate.opsForHash();
     }
 }
